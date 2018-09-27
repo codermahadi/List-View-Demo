9430a57592946145421692037229ac0119aaf3f0
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-09-28 00:51:46 +0600

    add list view

--- app/src/main/java/com/codem/mahadi/listviiewdemo/MainActivity.java
+++ app/src/main/java/com/codem/mahadi/listviiewdemo/MainActivity.java
@@ -2,12 +2,45 @@ package com.codem.mahadi.listviiewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myListView = (ListView) findViewById(R.id.myListVive);
+
+        ArrayList<String> myFamily = new ArrayList<String>();
+
+        myFamily.add("Mahadi");
+        myFamily.add("Fulmiya");
+        myFamily.add("Shahana");
+        myFamily.add("Bipasha");
+        myFamily.add("Arcel");
+        myFamily.add("Anika");
+        myFamily.add("Mahadi");
+        myFamily.add("Fulmiya");
+        myFamily.add("Shahana");
+        myFamily.add("Bipasha");
+        myFamily.add("Arcel");
+        myFamily.add("Anika");
+        myFamily.add("Mahadi");
+        myFamily.add("Fulmiya");
+        myFamily.add("Shahana");
+        myFamily.add("Bipasha");
+        myFamily.add("Arcel");
+        myFamily.add("Anika");
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
+
+        myListView.setAdapter(myAdapter);
     }
 }
